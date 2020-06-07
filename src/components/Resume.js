792f50bae1a13808021ca1dff4eb550bbb2b0743
shahid lashari <shahid_lashari001@yahoo.com>
2020-06-07 13:10:05 -0700

    updated skills css

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -67,7 +67,7 @@ export default  class Resume extends Component {
                     return(
                       <li>
                       <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
-                      </span><em>{item.skillname}</em>
+                      </span>* <em>{item.skillname}</em>
                       </li>
                     )
                   })
