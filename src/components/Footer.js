46e2333a8959f8148bd3cf3aeceb241d72f7126a
Shahid Lashari <shahid_lashari001@yahoo.com>
2023-03-12 21:02:26 -0700

    Update Footer.js
    updated year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ export default class Footer extends Component {
               })
             }
           </ul>
-          <large>&copy; Copyright 2021, Shahid Hussain </large>
+          <large>&copy; Copyright 2023, Shahid Hussain </large>
         </div>
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
       </div>
